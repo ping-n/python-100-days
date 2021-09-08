d3afe7ad6fa91f637575fef3daa2b1f2a6c1bd12
PING NGE <ping.nge@hotmail.com>
2021-09-08 23:58:26 +1000

    Update: Day 16 start

---
+++ day-16/turtle/main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+# Documentation for turtle module
+# https://docs.python.org/3/library/turtle.html
+
+speedy = Turtle()
+
+speedy.shape("turtle")
+speedy.color("blue", "cyan1")
+
+my_screen = Screen()
+
+my_screen.exitonclick()
