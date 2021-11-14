28b17418f580784f81211640300ea7e67592bed1
PING NGE <ping.nge@hotmail.com>
2021-11-14 23:11:33 +1100

    Update: Day 20 Snake Game

---
+++ snake_game/main.py
@@ -0,0 +1,20 @@
+from turtle import Screen, Turtle
+
+# Screen Setup
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
